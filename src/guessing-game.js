6f52a66f02528d4f51dda75b179878e58d734b52
Vitaliy Nikolaenko <vnickolaenko@gmail.com>
2022-08-06 20:53:51 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.array = []
+    }
 
     setRange(min, max) {
 
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.array = [];
+      let num = this.min;
+        for (let i = 0; i <= (this.max - this.min); i++) {
+            this.array.push(num);
+            num++
+        }
+      this.tryNumIndex = Math.trunc(this.array.length/2);
+      return this.array[Math.trunc(this.array.length/2)];
     }
 
     lower() {
-
+      this.setRange(this.min, this.array[this.tryNumIndex])
     }
 
     greater() {
-
+      this.setRange(this.array[this.tryNumIndex], this.max)
     }
 }
 
